4e26055b1f22cb3ead43150e53b393e9291a76c8
Glen-tech <glen_vp@hotmail.com>
2021-12-06 22:34:47 +0100

    Tutorial was a succes
    - Dataset added
    - Program works
    - Very accurate but a little slow
    - Can be expanded with machine learning

--- base.py
+++ base.py
@@ -9,8 +9,21 @@ Based on tutorial https://www.youtube.com/watch?v=PmZ29Vta7Vc
 
 import numpy as np
 import cv2 
+import pickle
 
 face_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_frontalface_alt2.xml')
+eye_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_eye.xml')
+smile_cascade = cv2.CascadeClassifier('cascades/data/haarcascade_smile.xml')
+
+
+recognizer = cv2.face.LBPHFaceRecognizer_create()
+recognizer.read("trainer.yml")
+
+lables= {}
+with open("labels.picle","rb") as f:
+    og_labels = pickle.load(f)
+    labels = {v:k for k,v in og_labels.items()}
+
 
 ESCAPE = 27
 ENTER = 13
@@ -29,7 +42,15 @@ while(True):
         
         # recognizer in deep learned model
         
-        
+        id_,conf= recognizer.predict(roi_gray) # label en conf
+        if conf >= 40 and conf <= 50:
+            print("conf:",conf)
+            print(id_)
+            print(labels[id_])
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            
+          
+         
         img_item = "my-image.png"
         img_item_color = "my-image-color.png"
         
@@ -42,8 +63,20 @@ while(True):
         end_cord_x = x + w
         end_cord_y = y + h
         cv2.rectangle(frame, (x,y), (end_cord_x, end_cord_y) , color , stroke)
+        cv2.putText(frame, labels[id_], (x,y), font , 2 , color=(255, 255, 255))
+            
         
+        #eyes = eye_cascade.detectMultiScale(roi_gray)
         
+        #for(ex,ey,ew,eh) in eyes:
+        #    cv2.rectangle(roi_color,(ex,ey), (ex+ew,ey+eh),color , stroke)
+        
+        #smile = smile_cascade.detectMultiScale(roi_gray)
+        
+        #for(ex,ey,ew,eh) in smile:
+        #    cv2.rectangle(roi_color,(ex,ey), (ex+ew,ey+eh),color , stroke)
+            
+            
     cv2.imshow('webcam',frame)
     
     key = cv2.waitKey(1)
